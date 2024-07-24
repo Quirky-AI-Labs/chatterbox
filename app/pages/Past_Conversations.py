3f3166769b5d031caac7a8597c2ce6bd060a89b9
Aayushshah196 <aayushshah.kanu@docsumo.com>
2024-07-24 22:49:39 +0545

    Added constructor for pages

--- app/pages/Past_Conversations.py
+++ app/pages/Past_Conversations.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
 from app.pages.base import BasePage
+from app.pages.utils import ConversationMixins
+from app.services.structures.conversation import ConversationList
 
 
-class PreviousConversationPage(BasePage):
+class PreviousConversationPage(BasePage, ConversationMixins):
     _title = "Previous Conversation"
+
+    def __init__(self, title, session, **kwargs):
+        super().__init__(title, session, **kwargs)
+        self.file_uploaded = False
+        self.file = None
+        self.model = None
+        self.conversations: ConversationList = ConversationList()
 
     def _display(self, **kwargs):
         rerun = True
